39ea5085e4040c040648700942f8df32109ca753
RudraPratapSingh10 <190636408+RudraPratapSingh10@users.noreply.github.com>
2026-07-28 21:35:08 +0530

    reuploaded

--- Search/App.jsx
+++ Search/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/router.js';
-import { SquadProvider } from './context/SquadContext.js';
-import { SettingsProvider } from './context/SettingsContext.js';
+import { router } from "./routes/router.jsx";
+import { SquadProvider } from './context/SquadContext.jsx';
+import { SettingsProvider } from './context/SettingsContext.jsx';
 
 export default function App() {
   return (
